f4520d249f26deb0a42a78305625e3dffca175fc
junmin0413 <wkdwnsals0413@gmail.com>
2025-09-22 16:25:40 +0900

    2025.09.22

---
+++ chap06/src/sec06/exam04/package1/B.java
@@ -0,0 +1,8 @@
+package sec06.exam04.package1;
+
+public class B {
+	//필드
+	A a1 = new A(true);
+	A a2 = new A(1); //같은 패키지는 default 접근제한 생성 사용 가
+	//A a3 = new A("문자열"); // private 생성자는 사용할 수 없음.
+}
